5779a7ffa7adef02a28b38ff7d57fb8cbcb6ef0a
maninderbali <bali.manni@gmail.com>
2017-01-17 00:57:45 +0530

    Checkbox practices

--- dist/dev/app/checkbox/checkbox.component.js
+++ dist/dev/app/checkbox/checkbox.component.js
@@ -1,23 +1,4 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require("@angular/core");
-var CheckboxComponent = (function () {
-    function CheckboxComponent() {
-        this.gender = 'male';
-    }
-    return CheckboxComponent;
-}());
-CheckboxComponent = __decorate([
-    core_1.Component({
-        selector: 'checkbox',
-        templateUrl: './app/checkbox/checkbox.component.html'
-    })
-], CheckboxComponent);
-exports.CheckboxComponent = CheckboxComponent;
-
+/**
+ * Created by root on 17/1/17.
+ */
 //# sourceMappingURL=checkbox.component.js.map
--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
 export class CheckboxComponent {
 
     gender: string = 'male';
+
+    maleCheckBox() {
+        console.log('checkbox click ');
+    }
 }
